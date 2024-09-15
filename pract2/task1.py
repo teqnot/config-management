4fe2111edce57a642e3caceef306325d1a6077fe
teqnot <56765759+teqnot@users.noreply.github.com>
2024-09-15 17:17:30 +0300

    Create task1.py

---
+++ pract2/task1.py
@@ -0,0 +1,7 @@
+import matplotlib
+
+print(f"Matplotlib version: {matplotlib.__version__}")
+
+print(f"Matplotlib location: {matplotlib.__file__}")
+
+print(f"Matplotlib description: {matplotlib.__doc__}")
